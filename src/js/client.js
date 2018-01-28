281da47def2255bc05e03ba77fff0e3d85c33d30
Rui Wang <rwa56@lib7000-rcmac04.lib.sfu.ca>
2018-01-27 18:20:12 -0800

    lesson 1

---
+++ src/js/client.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+import Layout from "./components/Layout";
+
+const app = document.getElementById('app');
+
+ReactDIM.render(<Layout/>, app);
